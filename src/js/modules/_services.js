31a5da07b8156dd4dd9c693dc14fe27d5d910c6b
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-09-24 22:44:22 +0300

    Find the way to add smooth animation for sevices block

--- src/js/modules/_services.js
+++ src/js/modules/_services.js
@@ -1,7 +1,31 @@
 const servicesParText = document.querySelector('.services__par-text'),
-    servicesReadmoreButton = document.querySelector('.services__readmore-button');
+    servicesTextWrap = document.querySelector('.services__text-wrap'),
+    servicesReadmoreButton = document.querySelector('.services__readmore-button'),
+    showBtn = document.querySelector('.services__readmore-button');
+
+let flag = true;
 
 servicesReadmoreButton.addEventListener('click', () => {
-    servicesParText.classList.toggle('services__par-text--opened');
+    showBtn.classList.toggle('services__readmore-button--active');
+
+    if(flag){
+        //insert inline style into screan element for smooth animation
+        servicesTextWrap.setAttribute('style', ` height: ${servicesParText.clientHeight}px;
+                                        transition: height;
+                                        transition-duration: 0.6s;
+                                        transition-timing-function: ease-in;`
+        );
+
+        showBtn.textContent = 'Скрыть';
+        flag = false;
+
+        return;
+    }
+
+    servicesTextWrap.removeAttribute('style');
+
+    showBtn.textContent = 'Показать все';
+
+    flag = true;   
 });
 
