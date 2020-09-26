d3e1b1789a5e0122a1d91a2e102b5a1c26eac5b6
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-09-26 15:06:17 +0300

    Add active style for menu navigation

--- src/js/modules/_menu-navigation.js
+++ src/js/modules/_menu-navigation.js
@@ -4,6 +4,25 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
 
+const menuNavigation = document.querySelector('.menu-navigation'),
+  servicesSliderItems = menuNavigation.querySelectorAll('.services-slider__item');
+
+menuNavigation.addEventListener('click', (event) => {
+  const target = event.target;
+
+  servicesSliderItems.forEach(item => {
+    if (item.classList.contains('services-slider__item--active')){
+      item.classList.remove('services-slider__item--active');
+    }
+  })
+
+  if (target.classList.contains('services-slider__item')){
+    target.classList.add('services-slider__item--active');
+  }else if (target.classList.contains('services-slider__text')) {
+    target.closest("div").classList.add('services-slider__item--active');
+  }
+})
+
 if (document.documentElement.clientWidth <= 1226){
     const menuNavigationSlider = new Swiper('.menu-navigation', {
         loop: false,
